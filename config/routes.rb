34839a4b6cfda8521c29a274f20cdac3570dfd64
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-06 20:52:48 +0900

    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
-  get 'items/create'
+  root to: 'items#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items
 end
