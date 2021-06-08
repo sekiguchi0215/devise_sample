b68956d3348c60c73176ee5c1e0fc50f9e158d8b
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-08 23:31:04 +0900

    ユーザー登録の際にニックネームを登録できるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_prameters, if: :devise_controller?
+
+  def configure_permitted_prameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
 end
